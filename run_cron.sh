83547754ae7bca2bc5fcd2a14c4534d7d1389333
Erik Olsson <haxx@erikolsson.dev>
2024-12-17 13:30:28 +0100

    WIO P# Please enter the commit message for your changes. Lines starting

--- run_cron.sh
+++ run_cron.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NODE_PATH=$(which node)
-export $(grep -v '^#' .env | xargs)
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-$NODE_PATH ./dist/index.js >> ./logs/output.log 2>&1
+export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
+$NODE_PATH "$SCRIPT_DIR/dist/index.js" >> "$SCRIPT_DIR/logs/output.log" 2>&1
