e3851ded14e1696e795acdd1d1ae2ae94a38e63f
Erik Olsson <haxx@erikolsson.dev>
2024-12-17 13:34:57 +0100

    WIP

--- run_cron.sh
+++ run_cron.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 NODE_PATH=$(which node)
+echo "Using Node version:"
+$NODE_PATH -v
+
+
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+
 
 export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
 $NODE_PATH "$SCRIPT_DIR/dist/index.js" >> "$SCRIPT_DIR/logs/output.log" 2>&1
