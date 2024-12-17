20966c1bfb76de4f0738970e4bb16c1c7fb533cf
Erik Olsson <haxx@erikolsson.dev>
2024-12-17 13:41:14 +0100

    WIP

--- run_cron.sh
+++ run_cron.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 NODE_PATH=$(which node)
-echo "Using Node version:"
-$NODE_PATH -v
-
-
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+# Debugging: Print Node.js version and working directory
+echo "Node.js Path: $NODE_PATH" >> "$SCRIPT_DIR/logs/debug.log"
+echo "Node.js Version:" >> "$SCRIPT_DIR/logs/debug.log"
+$NODE_PATH -v >> "$SCRIPT_DIR/logs/debug.log"
+echo "Working Directory: $(pwd)" >> "$SCRIPT_DIR/logs/debug.log"
 
-
-
+# Load environment variables and run the script
 export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
 $NODE_PATH "$SCRIPT_DIR/dist/index.js" >> "$SCRIPT_DIR/logs/output.log" 2>&1
